3d72b1103c51b5032b03b27198d820cc824bfbbe
Abhiram-Diriparupu <abhiramdiriparupu555@gmail.com>
2026-08-31 15:58:39 +0530

    comparison_operators.py

---
+++ comparison_operators.py
@@ -0,0 +1,15 @@
+#comparing operators 
+a=9
+b=7
+result=a==b #equal comparison operator
+print("result of",a,"==",b,"is:", result)
+result=a<b
+print("result of",a,"<",b,"is:",result)
+result=a>b
+print("result of",a,">",b,"is:",result)
+result=a>=b
+print("result of",a,">=",b,"is:",result)
+result=a<=b
+print("result of",a,"<=",b,"is:", result)
+result=a!=b
+print("result of",a,"!=",b,"is:", result)
