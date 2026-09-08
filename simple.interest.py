4c59df11115ef74b21cce486ae674d0da9d491df
Abhiram-Diriparupu <abhiramdiriparupu555@gmail.com>
2026-09-08 14:11:07 +0530

    Delete simple.interest.py
    delete the file

--- simple.interest.py
+++
@@ -1,6 +0,0 @@
-#CALCULATING SIMPLE INTEREST
-principle=int(input("enter the principle:") )
-rate = int(input("enter the rate in percentage:"))
-time = int(input("enter the time in years:"))
-interest = principle*time*rate/100
-print(interest)
