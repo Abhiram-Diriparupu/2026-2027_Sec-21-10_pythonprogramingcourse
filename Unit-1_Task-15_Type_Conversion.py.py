2f0f0b3da83e7c3943be1efb7cc403d31b25db9f
Abhiram-Diriparupu <abhiramdiriparupu555@gmail.com>
2026-09-25 14:47:08 +0530

    all type conversions

---
+++ Unit-1_Task-15_Type_Conversion.py.py
@@ -0,0 +1,20 @@
+#type conversions
+float_num = 99.9
+int_num = int(float_num)
+print(int_num)
+print(type(int_num))
+
+str_data="99"
+int_data=int(str_data)
+print(int_data)
+print(type(int_data))
+
+int_num = 99
+float_num = float(int_num)
+print(float_num)
+print(type(float_num))
+
+int_data=45
+str_data=str(int_data)
+print(str_data)
+print(type(str_data))
